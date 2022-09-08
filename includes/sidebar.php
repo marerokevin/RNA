af8a6b15fd61b015ea547bf78c031156c310e9c6
marerokevin <marerokevin@gmail.com>
2022-09-08 10:19:05 +0800

    resched

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
     <div class="sidebar" id="mySidebar">
         <ul>
             <li><a href="/RNA/K/index.php"><i class="fas fa-home"></i>Home</a></li>
-            <li><a href="/RNA/K/Risk-Miti.php?action=create"><i class="fas fa-project-diagram"></i>Risk Mitigation</a></li>
+            <li><a href="/RNA/K/Risk-Miti.php"><i class="fas fa-project-diagram"></i>Risk Mitigation</a></li>
             <li><a href="/RNA/K/directory.php"><i class="fas fa-address-book"></i>Employee Directory</a></li>
             <li><a href="#"><i class="fas fa-address-card"></i>About</a></li>
         </ul> 
