43f70eb822b1702fdcbc20f991fd04d0db0aea3d
marerokevin <marerokevin@gmail.com>
2022-10-17 16:00:07 +0800

    [Backend] To add update on request status

--- K/user/request2.php
+++ K/user/request2.php
@@ -1,22 +1,19 @@
 <?php
-    // include "./F/user/request.php";
-
 include "/includes/con/sess.php";
 include "/includes/D/config.php";
 
-
-
 if(isset($_POST["request"])) {
-
+    $String_b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $code = "GS";
     $get_month = date('m', strtotime("now"));
-    $number = 1;
+    $rand4 = substr(str_shuffle($String_b), 0, 4);
 
-    $generated = "$code$get_month-$number";
+    $generated = "$code$get_month-$rand4";
     $item = $_POST["item"];
     $supplier = $_POST["supplier"];
+    $unit = $_POST["uniteam"];
+    $price = $_POST["price"];
     $required_quantity = $_POST["required_amt"];
-    $unit = $_POST["unit"];
     $total = $_POST["total_amt"];
     $requestor = $_POST["requestor"];
 
@@ -24,33 +21,20 @@ if(isset($_POST["request"])) {
     $query_request_id = mysqli_query($db_conn, $check_request_id);
     $count_exist = mysqli_num_rows($query_request_id);
 
-    if($count_exist == "0") {
-        $request_insert = "INSERT INTO request (`item`, `supplier`, `required_quantity`, `unit`, `requestor`, `date_request`, `request_id`, `approval`, `status`, `total_price`) VALUES ('$item', '$supplier', '$required_quantity', '$unit', '$requestor', current_timestamp(), '$generated', false, false, '$total')";
+    if ($count_exist == "0") {
+        $request_insert = "INSERT INTO request (`item`, `unit`, `price`, `supplier`, `required_quantity`, `requestor`, `date_request`, `request_id`, `approval`, `status`, `total_price`) VALUES ('$item', '$unit', '$price', '$supplier', '$required_quantity', '$requestor', current_timestamp(), '$generated', false, false, '$total')";
         $request_insert_query = mysqli_query($db_conn, $request_insert);
-
-        if(!$request_insert_query) {
-            header("location: RNA/K/item-request.php?action=status");
-            echo '<script type="text/javascript"> alert("Information not updated") </script>';
-        }
-        if($request_insert_query) {
-            header("location: RNA/K/item-request.php?action=status");
-            echo '<script type="text/javascript"> alert("Done!") </script>';
-        }
-    }
-    elseif($count_exist != "0"){
-
+    }else {
+        $String_b='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $code = "GS";
         $get_month = date('m', strtotime("now"));
-        $xnumb = $number++;
+        $rand4 = substr(str_shuffle($String_b), 0, 4);
+        $generated_repeat = "$code$get_month-$rand4";
 
-        $generated2 = "$code$get_month-$count_exist";
-
-        $request_insert = "INSERT INTO request (`item`, `supplier`, `required_quantity`, `unit`, `requestor`, `date_request`, `request_id`, `approval`, `status`, `total_price`) VALUES ('$item', '$supplier', '$required_quantity', '$unit', '$requestor', current_timestamp(), '$generated2', false, false, '$total')";
+        $request_insert = "INSERT INTO request (`item`, `unit`, `price`, `supplier`, `required_quantity`, `requestor`, `date_request`, `request_id`, `approval`, `status`, `total_price`) VALUES ('$item', '$unit', '$price', '$supplier', '$required_quantity', '$requestor', current_timestamp(), '$generated_repeat', false, false, '$total')";
         $request_insert_query = mysqli_query($db_conn, $request_insert);
     }
 }
-echo $generated;
-
 ?>
 
 <!DOCTYPE html>
@@ -91,11 +75,11 @@ echo $generated;
                     <div class="input-container">
                         <select type="text" class="input-main" id="item" name="item" onchange="itemSelect(this)" required>
                             <option value="" disabled selected>Select item</option>
-                            <?php  
+                            <?php
                                 $list_supplier = "select unit, unit_price, description, item, supplier from inventory";
                                 $item_query = mysqli_query($db_conn, $list_supplier);
                                 while ($items = mysqli_fetch_assoc($item_query)) {
-                                    echo '<option id="'.$items["item"].'" data-val="'.$items["supplier"].'" data-val="'.$items["item"].'" data-item-price="'.$items["unit_price"].'" value="'.$items["item"].'">'.$items["item"].' - '.$items["description"].'</option>';
+                                    echo '<option id="'.$items["item"].'" data-val="'.$items["supplier"].'" data-val="'.$items["item"].'" data-item-unit="'.$items["unit"].'" data-item-price="'.$items["unit_price"].'" value="'.$items["item"].'">'.$items["item"].' - '.$items["description"].'</option>';
                                     $item = $items["item"];
                                 }
                             ?>
@@ -104,7 +88,6 @@ echo $generated;
                 </div>
                 <span class="number">2</span>
                 <label for="start-grid" class="input-label">Quantity</label>
-                <div class="start-container" id="start-grid">
                     <!-- Required Amount -->
                     <div class="start-container" id="start-grid">
                         <label for="required_amt" class="input-label">Required Amount</label>
@@ -114,10 +97,12 @@ echo $generated;
                     </div>
 
                     <!-- Total Amount -->
+                <div class="start-container" id="start-grid">
                     <div class="start-container" id="start-grid">
                         <label for="start-grid" class="input-label">Unit Price</label>
                         <div class="input-container"> 
-                            <input type="text" class="input-main" name="price" id="price" placeholder="Price" oninput="multiply()">
+                            <input type="text" class="input-main" name="price" id="price" placeholder="Price" oninput="multiply()"> / 
+                            <input type="text" class="input-main" name="uniteam" id="uniteam" placeholder="Unit">
                         </div>
                     </div>
                 </div>
@@ -129,15 +114,7 @@ echo $generated;
                         <input class="input-main" id="total_amt" name="total_amt" readonly>
                     </div>
                 </div>
-
-                <!-- Request_ID -->
-                <!-- <label for="start-grid" class="input-label">Request Number</label> -->
-                <!-- <div class="start-container" id="start-grid"> -->
-                    <!-- <div class="input-container">  -->
-                        <!-- <input type="text" class="input-main" id="request_id" placeholder="Request Number" -->
-                        <!-- name="request_id" required onblur="gatherDataAndOutput()" readonly autofocus=""> -->
-                    <!-- </div> -->
-                <!-- </div> -->
+                
 
                 <!-- Encoded By -->      
                 <label for="start-grid" class="input-label">Encoded by</label>
@@ -180,8 +157,8 @@ $('#supplier').change(function() {
 })
 
 function itemSelect(data) {
-const pariah = document.getElementById(document.getElementById("item").value).dataset.itemPrice;
 document.getElementById("price").value = document.getElementById(document.getElementById("item").value).dataset.itemPrice;
+document.getElementById("uniteam").value = document.getElementById(document.getElementById("item").value).dataset.itemUnit;
 }
 
 function multiply() {
@@ -191,6 +168,5 @@ function multiply() {
     document.getElementById('price').innerHTML = multiplicand;
     document.getElementById('required_amt').innerHTML = multiplier;
     document.getElementById('total_amt').value = product;
-    console.log(multiplicand);
 }
 </script>
