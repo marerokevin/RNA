1ba00288c3c5dea0e935e771824876fe5d2b80ad
marerokevin <marerokevin@gmail.com>
2022-11-25 12:23:20 +0800

    filter on registration

--- K/user/status.php
+++ K/user/status.php
@@ -19,7 +19,7 @@
           </tr>
         </thead>
         <tbody class="table-body-container"> 
-          <?php 
+			<?php 
             include ("../includes/d/config.php");
             $first_name = $_SESSION["first_name"];
             $last_name = $_SESSION["last_name"];
@@ -45,44 +45,28 @@
                 <td class="body-data"><?php echo $request['total_price']; ?></td>
                 <td class="body-data"><?php echo $request['requestor'];?></td>
                 <td class="body-data">
-                  <?php
+				<?php
                     if ($request['approval'] == False) { //waiting for approval
-                      if ($_SESSION["user_level"] == "Administrator") { //Waiting for approval(as admin)
-                        echo '<a class="data-action" href="/RNA/K/admin/approve.php?request='.$request['request_id'].'">For Approval of'. $request['approver'] .'</a>'; //approval button
-                      }elseif ($_SESSION["user_level"] == "Head") {
+						if ($_SESSION["user_level"] == "Administrator") { //Waiting for approval(as admin)
+							echo '<a class="data-action" href="/RNA/K/admin/approve.php?request='.$request['request_id'].'">For Approval of '. $request['approver'] .'</a>'; //approval button
+						}elseif ($_SESSION["user_level"] == "Head") {
                         if ($user != $request['approver']) {//Waiting for approval(as Head) ?>
-                          <a class="data-action" href="/RNA/K/head/approve.php?request=<?php echo $request['request_id']; ?>">For approval of <?php echo $request['approver']; ?></a> <?php
+                          	<a class="data-action" href="/RNA/K/head/approve.php?request=<?php echo $request['request_id']; ?>">For approval of <?php echo $request['approver']; ?></a> <?php
                         }elseif ($user == $request['approver']) { ?>
-                          <a class="data-action" href="/RNA/K/head/approve.php?request=<?php echo $request['request_id']; ?>">For your approval</a> <?php
+                          	<a class="data-action" href="/RNA/K/head/approve.php?request=<?php echo $request['request_id']; ?>">For your approval</a> <?php
                         }
-                      }elseif ($_SESSION["user_level"] == "User") {
-                        echo "For approval of "; echo $request['approver']; //Display for approval on user
-                      }
-                    }elseif ($request['approval'] == True && $request['status'] == True) { //approved
-                      echo "Approved"; //message if approval = true
-                    }
+					}elseif ($_SESSION["user_level"] == "User") {
+						echo "For approval of "; echo $request['approver']; //Display for approval on user
+					}
+				}elseif ($request['approval'] == True && $request['status'] == True) { //approved
+					echo "Approved"; //message if approval = true
+				}
                 ?>
-              </td>
-          </tr>
-          <?php } ?>
+              	</td>
+          	</tr>
+         	 <?php } ?>
         </tbody>
       </table>
     </div>
   </div>
 </body>
-
-<script>
-  function lgbkSav3Btn(){
-            if(nameVal.value == ""){
-                swal ( "Oops" ,  "You have entered an invalid Employee Name!" ,  "error" );
-            }else if(empSel.selectedIndex === 0){
-                swal ( "Oops" ,  "You have selected an invalid Employer!" ,  "error" );
-            }else{
-                if(document.getElementById("lgbkFormTitle").innerHTML == "ADD"){
-                    document.getElementById("lgbkAdd").click();
-                }else if(document.getElementById("lgbkFormTitle").innerHTML == "EDIT"){
-                    document.getElementById("lgbkEdit").click();
-                }
-            }
-        }
-</script>
--- includes/bar/sidebar.php
+++ includes/bar/sidebar.php
@@ -4,7 +4,7 @@
         <?php $useraccess = $_SESSION["user_level"]; ?>
             <li><a href="/RNA/K/index.php"><i class="fas fa-home"></i>Dashboard</a></li>
             <li><a href="/RNA/K/item-request.php?action=status"><i class="fas fa-shopping-cart"></i>Item Request</a></li>
-            <li><a href="/RNA/K/Invento1ry.php"><i class="fas fa-box-open"></i>Inventory</a></li>
+            <li><a href="/RNA/K/Inventory.php"><i class="fas fa-box-open"></i>Inventory</a></li>
         </ul> 
     </div>
 </div>
